0e73548dbb0a00aa1ed7238e2c927f670eb968aa
CroisetEric <53318863+CroisetEric@users.noreply.github.com>
2020-11-27 18:11:19 +0100

    wdt.py v2
    Fixed Paths and sendmail executale path for cronjob

--- wdt.py
+++ wdt.py
@@ -4,8 +4,7 @@ from datetime import datetime
 import time
 from bs4 import BeautifulSoup
 import os
-#Wort des Tages Duden Crawler mit Cronjob 0 8 * * * /usr/bin/python3 /home/mgmtadmin/wdt/wdt.py
-#Sendmail with Iptables block Ports 25 587
+
 #class="scene__title-link"
 
 url = 'https://www.duden.de/wort-des-tages'
@@ -22,11 +21,9 @@ message = word + ' - ' + wordexp
 
 now = datetime.now()
 dt_string = now.strftime("%d-%m-%Y-%H-%M-%S")
-myfile = "wdt-" + dt_string
-
+myfile = "/home/mgmtadmin/wdt/wdt-" + dt_string
 f = open(myfile, "w")
 f.write("Subject:"+word+"\n"+message+"\n")
 f.close()
-
-sendmail = 'sendmail croiset.eric@gmail.com < /home/mgmtadmin/wdt/' + myfile
+sendmail = '/usr/sbin/sendmail croiset.eric@gmail.com < ' + myfile
 os.system(sendmail)
